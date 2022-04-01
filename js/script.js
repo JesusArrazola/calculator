3d81f92dc7ada61b3e8574b9b87e529d61abe4e3
=Jesus Arrazola <arrazola69@hotmail.com>
2022-03-31 23:20:32 -0600

    Item 3 done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
 //properties
-let display = "";
+//Objects
+let txtDisplay = document.querySelector('input#txtDisplay');
+let btnNumbers = document.querySelectorAll('button.number');
+let btnOperators = document.querySelectorAll('.operator[id]');
+let btnClear = document.querySelector('button.clear-button');
+let btnPeriod = document.querySelector('button.operator#period');
+let display = txtDisplay.value;
+
+//Display functions
+const displayWrite = (mode,text)=>{
+    switch(mode){
+        case "concatenate":
+            txtDisplay.value += text;
+        break;
+        case "rewrite":
+            txtDisplay.value = text;
+        break;
+        case "clear":
+            txtDisplay.value = "";
+        break;
+    }
+    display = txtDisplay.value;
+}
+//Attacht listener to buttons
+btnNumbers.forEach(e=>{
+    e.addEventListener('click',(evt)=>{
+        let mode = display==='0'?'rewrite':'concatenate';
+        if(display.length < 8)
+            displayWrite(mode,evt.target.textContent);
+    });
+});
+
+btnClear.addEventListener('click',()=>{
+    displayWrite('rewrite','0');
+});
+
+btnPeriod.addEventListener('click',()=>{
+    if(!display.includes('.'))
+        if(display === '0')
+            displayWrite('rewrite','0.');
+        else
+            displayWrite('concatenate','.');
+});
 
 //Basic functions
 const add = (n1,n2) =>{
